db4a46775c04bd54b05db19c5c72fd5f04b94a02
Will Allan <billyzkid@yahoo.com>
2017-01-30 00:53:53 -0800

    Update

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ const prompt = "> ";
 const terminal = new Terminal({ cursorBlink: true });
 
 terminal.open(document.getElementById("app"));
-terminal.write(`\x1b[?25l\r\x1b[0m${story.title}\x1b[0K\r\n\x1b[0K\r\n${prompt}\x1b[0K\r\x1b[${prompt.length + 1}G\x1b[?25h`);
+terminal.write(`\x1b[?25l\r\x1b[0m${story.title}\x1b[0K\r\n\x1b[0K\r\n${prompt}\x1b[0K\x1b[?25h`);
 
 let input = "";
 
 terminal.on("data", (data) => {
-  if (data === "\r") {
+  if (data === "\x0d") {
     const output = story.ask(input);
     input = "";
-    terminal.write(`\x1b[?25l\r\n${output}\x1b[0K\r\n\x1b[0K\r\n${prompt}\x1b[0K\r\x1b[${prompt.length + 1}G\x1b[?25h`);
+    terminal.write(`\x1b[?25l\r\n${output}\x1b[0K\r\n\x1b[0K\r\n${prompt}\x1b[0K\x1b[?25h`);
+  } else if (data === "\x7f") {
+    input += "\b \b";
+    terminal.write(`\x1b[?25l\r${prompt}${input}\x1b[0K\x1b[?25h`);
   } else {
     input += data;
-    terminal.write(`\x1b[?25l\r${prompt}${input}\x1b[0K\r\x1b[${(prompt + input).length + 1}G\x1b[?25h`);
+    terminal.write(`\x1b[?25l\r${prompt}${input}\x1b[0K\x1b[?25h`);
   }
 });
